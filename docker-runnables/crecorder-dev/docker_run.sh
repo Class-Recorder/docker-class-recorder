6fa93bedbcee43f0683253d373872095aa7bb315
cruizba <charlesrb94@outlook.com>
2018-12-16 01:41:20 +0100

    crecorder-dev docker compose waits to mysql

--- docker-runnables/crecorder-dev/docker_run.sh
+++ docker-runnables/crecorder-dev/docker_run.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 xhost local: 
 echo "Opened xhost to local connections"
 export AUDIO_GROUP_CR=$(getent group audio | cut -d: -f3)
 echo "Created AUDIO_GROUP_CR environment variable to ${AUDIO_GROUP_CR}"
 export CURRENT_UID=$(id -u):$(id -g)
-mkdir class-recorder
+mkdir -p class-recorder/class-recorder
 docker-compose up
