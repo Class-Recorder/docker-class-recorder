c66ecef2c4939e3b28143bfd82f8cdfa52712096
cruizba <charlesrb94@outlook.com>
2018-12-26 22:37:22 +0100

    Updated travis

--- travis.sh
+++ travis.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build docker-images/crecorder-base -t cruizba/crecorder-base
+docker build docker-images/crecorder-dev -t cruizba/crecorder-dev
+docker build docker-images/crecorder-ci -t cruizba/crecorder-ci
 docker push cruizba/crecorder-base
+docker push cruizba/crecorder-dev
+docker push cruizba/crecorder-ci
