fafadcb0d2920764a0e2ffd0a3fffb6086a923f1
cruizba <charlesrb94@outlook.com>
2018-12-27 18:33:47 +0100

    minor changes

--- docker-runnables/crecorder-ci/docker_run.sh
+++ docker-runnables/crecorder-ci/docker_run.sh
@@ -3,13 +3,8 @@ xhost local:
 echo "Opened xhost to local connections"
 export AUDIO_GROUP_CR=$(getent group audio | cut -d: -f3)
 echo "Created AUDIO_GROUP_CR environment variable to ${AUDIO_GROUP_CR}"
-export CURRENT_UID=$(id -u):$(id -g)
-echo $CURRENT_UID
-mkdir -p class-recorder/class-recorder
 docker-compose up -d
 sleep 20
-
-docker exec -it $(docker container ls -q -l) bash -c "export HOME=/home/travis/build/Class-Recorder/class-recorder && echo $HOME"
 
 echo "Cloning repository"
 docker exec -it $(docker container ls -q -l) bash -c "git clone -b $TRAVIS_BRANCH https://github.com/Class-Recorder/class-recorder"
