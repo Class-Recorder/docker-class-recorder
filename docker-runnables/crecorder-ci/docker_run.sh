38f395b73ab71e0d618a542858dcccfc366c301a
cruizba <charlesrb94@outlook.com>
2018-12-27 15:29:28 +0100

    minor changes

--- docker-runnables/crecorder-ci/docker_run.sh
+++ docker-runnables/crecorder-ci/docker_run.sh
@@ -4,6 +4,7 @@ echo "Opened xhost to local connections"
 export AUDIO_GROUP_CR=$(getent group audio | cut -d: -f3)
 echo "Created AUDIO_GROUP_CR environment variable to ${AUDIO_GROUP_CR}"
 export CURRENT_UID=$(id -u):$(id -g)
+echo $CURRENT_UID
 mkdir -p class-recorder/class-recorder
 docker-compose up -d
 sleep 20
@@ -11,7 +12,7 @@ sleep 20
 echo "Cloning repository"
 docker exec -it $(docker container ls -q -l) bash -c "git clone -b $TRAVIS_BRANCH https://github.com/Class-Recorder/class-recorder"
 echo "Installing dependencies"
-docker exec -it $(docker container ls -q -l) bash -c "cd class-recorder && ls -l && sudo npm install && sudo npm run install-dependencies && sudo npm run install-dependencies-cordova"
+docker exec -it $(docker container ls -q -l) bash -c "cd class-recorder && ls -l && npm install && npm run install-dependencies && npm run install-dependencies-cordova"
 docker exec -it $(docker container ls -q -l) bash -c "cd class-recorder && ls -l"
 echo "Executing test"
 docker exec -it $(docker container ls -q -l) bash -c "cd class-recorder && npm run test-pc-server && npm run test-pc-frontend && npm run build"
