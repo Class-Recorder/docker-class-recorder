92c57742f9fb980d2d39ced34b8556f154d3f578
cruizba <charlesrb94@outlook.com>
2018-12-29 01:09:50 +0100

    Auto release for class-recorder

--- docker-runnables/crecorder-ci/docker_run.sh
+++ docker-runnables/crecorder-ci/docker_run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+RELEASE_DIR="/tmp/release"
+WORKDIR=$(pwd)
+
 xhost local: 
 echo "Opened xhost to local connections"
 export AUDIO_GROUP_CR=$(getent group audio | cut -d: -f3)
@@ -14,4 +18,25 @@ echo "Executing test"
 docker exec -it $(docker container ls -q -l) bash -c "cd class-recorder && npm run test-pc-server && npm run test-pc-frontend"
 echo "Building release"
 docker exec -it $(docker container ls -q -l) bash -c "cd class-recorder && npm run build"
-ls -l
+
+# Creating release
+mkdir $RELEASE_DIR
+cp class-recorder/class-recorder/build-binaries/class-recorder-pc.jar $RELEASE_DIR
+cp class-recorder/class-recorder/build-binaries/class-recorder.apk $RELEASE_DIR
+zip $RELEASE_DIR/[Docker]-crecorder-pc-prod-mysql.zip  ../crecorder-pc-prod-mysql
+zip $RELEASE_DIR/[Docker]-crecorder-pc-prod-h2.zip  ../crecorder-pc-prod-h2
+
+# Copy of produced jar to build new image
+cd ../../docker-images/crecorder-pc-prod/
+cp $RELEASE_DIR/class-recorder-pc.jar .
+
+# Docker login and push of crecorder-pc-prod
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build . -t cruizba/crecorder-pc-prod
+docker push cruizba/crecorder-pc-prod
+
+# Copy release folder in workdir
+cd $WORKDIR
+cp -R $RELEASE_DIR $WORKDIR
+
+
---
+++ docker-runnables/crecorder-pc-prod-mysql/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+xhost +local: 
+echo "Opened xhost to local connections"
+export AUDIO_GROUP_CR=$(getent group audio | cut -d: -f3)
+echo "Created AUDIO_GROUP_CR environment variable to ${AUDIO_GROUP_CR}"
+export CURRENT_UID=$(id -u):$(id -g)
+mkdir mysql && mkdir videos
+docker-compose up
