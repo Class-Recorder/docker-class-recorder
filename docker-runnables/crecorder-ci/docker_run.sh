e6a8fd04477a9758f7ce7b815b568de108e15c5a
cruizba <charlesrb94@outlook.com>
2018-12-31 17:35:23 +0100

    Automatic tags for docker production images

--- docker-runnables/crecorder-ci/docker_run.sh
+++ docker-runnables/crecorder-ci/docker_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# ----- Environment setup -----
 RELEASE_DIR="/tmp/release"
 WORKDIR=$(pwd)
 
@@ -7,9 +8,10 @@ xhost local:
 echo "Opened xhost to local connections"
 export AUDIO_GROUP_CR=$(getent group audio | cut -d: -f3)
 echo "Created AUDIO_GROUP_CR environment variable to ${AUDIO_GROUP_CR}"
-docker-compose up -d
+docker-compose up -d # Run infrastructure
 sleep 20
 
+# ----- Testing and building -----
 echo "Cloning repository"
 docker exec -it $(docker container ls -q -l) bash -c "git clone -b $TRAVIS_BRANCH https://github.com/Class-Recorder/class-recorder"
 echo "Installing dependencies"
@@ -19,25 +21,29 @@ docker exec -it $(docker container ls -q -l) bash -c "cd class-recorder && npm r
 echo "Building release"
 docker exec -it $(docker container ls -q -l) bash -c "cd class-recorder && npm run build"
 
-# Creating release
+# ----- Creating release -----
 mkdir $RELEASE_DIR
 cp class-recorder/class-recorder/build-binaries/class-recorder-pc.jar $RELEASE_DIR
 cp class-recorder/class-recorder/build-binaries/class-recorder.apk $RELEASE_DIR
 ls -l ../crecorder-pc-prod-mysql
 ls -l ../crecorder-pc-prod-h2
 cd ..
-zip -r $RELEASE_DIR/Docker-crecorder-pc-prod-mysql.zip  crecorder-pc-prod-mysql
+
+#Adapting docker-compose to specified tag for production docker-images
+sudo add-apt-repository ppa:rmescandon/yq 
+sudo apt update
+sudo apt install yq -y # Install yq to modify docker-compose.yml
+yq write --inplace  crecorder-pc-prod-h2/docker-compose.yml services.teacher-pc-server.image cruizba/crecorder-pc-prod:$TRAVIS_TAG
+yq write --inplace  crecorder-pc-prod-mysql/docker-compose.yml services.teacher-pc-server.image cruizba/crecorder-pc-prod:$TRAVIS_TAG
+
+# Zipping docker runnables for releases
+zip -r $RELEASE_DIR/Docker-crecorder-pc-prod-mysql.zip crecorder-pc-prod-mysql
 zip -r $RELEASE_DIR/Docker-crecorder-pc-prod-h2.zip  crecorder-pc-prod-h2
 cd $WORKDIR
 
 # Copy of produced jar to build new image
 cd ../../docker-images/crecorder-pc-prod/
 cp $RELEASE_DIR/class-recorder-pc.jar .
-
-# Docker login and push of crecorder-pc-prod
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build . -t cruizba/crecorder-pc-prod
-docker push cruizba/crecorder-pc-prod
 
 # Copy release folder in workdir
 cd $WORKDIR
