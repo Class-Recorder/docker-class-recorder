2512df929d8d090d95e60aadb210fcdc208aedd5
cruizba <charlesrb94@outlook.com>
2019-01-01 22:31:56 +0100

    Added folder .credentials for youtube

--- docker-runnables/crecorder-pc-prod-mysql/run.sh
+++ docker-runnables/crecorder-pc-prod-mysql/run.sh
@@ -4,5 +4,5 @@ echo "Opened xhost to local connections"
 export AUDIO_GROUP_CR=$(getent group audio | cut -d: -f3)
 echo "Created AUDIO_GROUP_CR environment variable to ${AUDIO_GROUP_CR}"
 export CURRENT_UID=$(id -u):$(id -g)
-mkdir mysql && mkdir videos && mkdir temp
+mkdir mysql && mkdir videos && mkdir temp && mkdir .credentials
 docker-compose up
